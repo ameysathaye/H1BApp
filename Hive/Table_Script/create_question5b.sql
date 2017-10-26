6dd3667fbb2261faf9c22a7796352ccfde1d549d
ameysathaye <33028687+ameysathaye@users.noreply.github.com>
2017-10-26 05:13:05 +0000

    Create create_question5b.sql

---
+++ Hive/Table_Script/create_question5b.sql
@@ -0,0 +1,5 @@
+create table niit_h1b.question5b(job_title string,              	                    
+year string,              	                    
+case_status string,              	                    
+count  bigint
+);
