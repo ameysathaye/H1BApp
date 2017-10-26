e9954ff7235fc1e8a74d76ffd31f8f4e5cb5837a
ameysathaye <33028687+ameysathaye@users.noreply.github.com>
2017-10-26 05:04:54 +0000

    Update create_question2b.sql

--- Hive/Table_Script/create_question2b.sql
+++ Hive/Table_Script/create_question2b.sql
@@ -1,4 +1,4 @@
-create table question2b(worksite string,           	                    
+create table niit_h1b.question2b(worksite string,           	                    
 count bigint,              	                    
 year string              	                    
 );
