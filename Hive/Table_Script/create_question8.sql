55f5860035999cfead1dc63523355ae0717c6eb0
ameysathaye <33028687+ameysathaye@users.noreply.github.com>
2017-10-26 05:19:56 +0000

    Create create_question8.sql

---
+++ Hive/Table_Script/create_question8.sql
@@ -0,0 +1,5 @@
+create table niit_h1b.question8(job_title string,              	                    
+full_time_position  string,              	                    
+year string,              	                    
+avg_wage bigint              	                    
+); 	 
